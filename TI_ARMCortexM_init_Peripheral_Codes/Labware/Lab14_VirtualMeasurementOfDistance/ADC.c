3ee9cf92a49cd9031efb294de1803177db99a2c9
Mahad Jamal <mjamal@bionx.local>
2017-12-24 14:53:41 -0500

    Intitalizing PE2 ->AIN1 on TM4C to take ADC input

--- TI_ARMCortexM_init_Peripheral_Codes/Labware/Lab14_VirtualMeasurementOfDistance/ADC.c
+++ TI_ARMCortexM_init_Peripheral_Codes/Labware/Lab14_VirtualMeasurementOfDistance/ADC.c
@@ -31,7 +31,36 @@
 // SS3 1st sample source:  channel 1
 // SS3 interrupts: enabled but not promoted to controller
 void ADC0_Init(void){ 
- 
+  volatile unsigned long delay;
+//  SYSCTL_RCGC0_R |= 0x00010000; // 1) activate ADC0 (legacy code)
+  SYSCTL_RCGCADC_R |= 0x00000001; // 1) activate ADC0
+  SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4; // 1) activate clock for Port E
+  delay = SYSCTL_RCGCGPIO_R;      // 2) allow time for clock to stabilize
+  delay = SYSCTL_RCGCGPIO_R;
+	GPIO_PORTE_AMSEL_R = 0x04;	
+  GPIO_PORTE_DIR_R &= ~0x04;         // 5) PE2 IN
+  GPIO_PORTE_AFSEL_R |= 0x04;      // 6) disable alt funct on PE2
+  GPIO_PORTE_DEN_R &= ~0x04;         // 7) disable digital I/O on PE2
+	
+	GPIO_PORTE_PCTL_R &= ~0xF0000000; // 4) PCTL GPIO on PA7
+	
+	ADC0_PC_R &= ~0xF;              // 8) clear max sample rate field
+  ADC0_PC_R |= 0x1;               //    configure for 125K samples/sec
+  ADC0_SSPRI_R = 0x0123;          // 9) Sequencer 3 is highest priority //Sequence prioritizer
+  ADC0_ACTSS_R &= ~0x0008;        // 10) disable sample sequencer 3
+  ADC0_EMUX_R &= ~0xF000;         // 11) seq3 is software trigger //ADC is initited by setting SSn bit in PSSI
+  
+	//ADC0_SSMUX3_R = 0x1;         
+  ADC0_SSMUX3_R &= ~0x000F;       // 11.1) clear SS3 field
+  ADC0_SSMUX3_R += 1;             //    set channel Ain1 (PE2). 12) set channel AIN1->PE2 for SS3. 8 possible. Only using
+	
+	ADC0_SSCTL3_R = 0x0006;         // 13)  no TS0 D0, yes IE0 END0 // 0 1 1 0 //First sample
+	
+  ADC0_IM_R &= ~0x0008;           // 14) disable SS2 interrupts // Interrupt MASK // The status of SS3 in ADC0 does not effect its interrupt status
+	
+  ADC0_ACTSS_R |= 0x0008;         // 13) enable sample sequencer 3 //ADC0 SS3 Enable// 16 bit to show of ADC is busy or idle
+	
+	
 }
 
 
